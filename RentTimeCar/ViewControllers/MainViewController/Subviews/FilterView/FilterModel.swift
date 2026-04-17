194f8f9dc513cfdfe150451595e38cde1b113bb7
Ivan Shishkin <ivan23-1@yandex.ru>
2026-04-17 14:16:07 +0300

    fix filter model

--- RentTimeCar/ViewControllers/MainViewController/Subviews/FilterView/FilterModel.swift
+++ RentTimeCar/ViewControllers/MainViewController/Subviews/FilterView/FilterModel.swift
@@ -29,7 +29,7 @@ extension FilterModel {
             FilterModel(
                 type: .filter,
                 image: .filter,
-                text: "Фильтр"
+                text: "Марка авто"
             ),
             FilterModel(
                 type: .date,
