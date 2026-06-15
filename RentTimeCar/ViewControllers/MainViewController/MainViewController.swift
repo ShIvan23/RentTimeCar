432dcc7f3d0a8c7e249092c66e1b003f3a47185d
Ivan Shishkin <ivan23-1@yandex.ru>
2026-06-15 15:20:44 +0300

    Fix app icon. Release 2

--- RentTimeCar/ViewControllers/MainViewController/MainViewController.swift
+++ RentTimeCar/ViewControllers/MainViewController/MainViewController.swift
@@ -100,7 +100,7 @@ final class MainViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        navigationController?.setNavigationBarHidden(true, animated: false)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
     }
 
     override func viewDidLayoutSubviews() {
